2d1ef7f1f14e73a1e6b27968b30a32b7b9bbc031
proger-n <ahmetgarievnail@gmail.com>
2022-06-29 22:06:04 +0300

    added lec_4 and modified 1_fibonaci algrithm

--- 1_fibonaci.py
+++ 1_fibonaci.py
@@ -1,18 +1,33 @@
-def fib(n):
-    # put your code here
+# def fib(n):
+#     # put your code here
+#     f1, f2 = 0, 1
+#     for i in range(1, n + 1):
+#         f1, f2 = f2, f1 + f2
+#
+#     return f2
+#
+# def main():
+#     n = int(input())
+#     print(fib(n))
+#
+# main()
+
+def fib_digit(n):
     f1 = 0
     f2 = 1
     fibon = 0
+    digit = 0
     for i in range(1, n + 1):
         fibon = f1 + f2
+        digit = (fibon % 10 + f2 % 10) % 10
         f2 = f1
         f1 = fibon
 
-    return fibon
+    return digit
+
 
 def main():
     n = int(input())
-    print(fib(n))
+    print(fib_digit(n))
 
 main()
-
