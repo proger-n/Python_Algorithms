ec7f8416d0106b157e7c63491cb77deaf741da5b
proger-n <ahmetgarievnail@gmail.com>
2022-06-30 08:38:29 +0300

    updated fibonacci, solved two questions

--- 1_fibonaci.py
+++ 1_fibonaci.py
@@ -1,29 +1,32 @@
-# def fib(n):
-#     # put your code here
-#     f1, f2 = 0, 1
-#     for i in range(1, n + 1):
-#         f1, f2 = f2, f1 + f2
-#
-#     return f2
-#
-# def main():
-#     n = int(input())
-#     print(fib(n))
-#
-# main()
+# 1. Дано число n, от 1 до 40
+# необходимо вычислить n-е число Фибоначчи
+
+def fib(n):
+    # put your code here
+    f1, f2 = 0, 1
+    for i in range(1, n):
+        f1, f2 = f2, f1 + f2
+
+    return f2
+
+def main():
+    n = int(input())
+    print(fib(n))
+
+main()
+
+
+
+# 2. Дано число n, от 1 до 10^7
+# необходимо найти последнюю цифру n-го числа Фибоначчи, за менее чем 3 секунды
 
 def fib_digit(n):
-    f1 = 0
-    f2 = 1
-    fibon = 0
+    f1, f2 = 0, 1
     digit = 0
-    for i in range(1, n + 1):
-        fibon = f1 + f2
-        digit = (fibon % 10 + f2 % 10) % 10
-        f2 = f1
-        f1 = fibon
+    for i in range(1, n):
+        f1, f2 = f2 % 10, (f1 + f2) % 10
 
-    return digit
+    return f2
 
 
 def main():
